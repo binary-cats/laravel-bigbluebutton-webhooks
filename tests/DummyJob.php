2c4501525708671dacf6b6d69e895d7e4c130058
Cyrill N. Kalita <cyrill.kalita@gmail.com>
2026-03-20 19:04:34 -0500

    Add GitHub Actions for dependency updates and tests

--- tests/DummyJob.php
+++ tests/DummyJob.php
@@ -8,16 +8,9 @@ class DummyJob
 {
     /**
      * Bind the implementation.
-     *
-     * @var \Spatie\WebhookClient\Models\WebhookCall
      */
     public WebhookCall $webhookCall;
 
-    /**
-     * Create new Job.
-     *
-     * @param  \Spatie\WebhookClient\Models\WebhookCall  $webhookCall
-     */
     public function __construct(WebhookCall $webhookCall)
     {
         $this->webhookCall = $webhookCall;
@@ -25,10 +18,8 @@ class DummyJob
 
     /**
      * Handle the job.
-     *
-     * @return void
      */
-    public function handle()
+    public function handle(): void
     {
         cache()->put('dummyjob', $this->webhookCall);
     }
